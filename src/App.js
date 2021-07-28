8cf7f14f929971a824976f4fe5294cb624913fec
harsh71019 <harsh710patel@gmail.com>
2021-07-28 16:44:26 +0530

    initial redux and thunk boilerplate complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import BoilerScreen from "./screens/BoilerScreen";
+import Container from "react-bootstrap/Container";
+
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <Router>
+      <Container>
+        <Route path="/" component={HomeScreen} exact></Route>
+        <Route path="/test" component={BoilerScreen}></Route>
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
